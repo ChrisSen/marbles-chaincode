e9bcbdb93a590069ac73f248d98529bdeb64eda9
ChrisSen <chrissen@example.com>
2016-09-05 09:49:28 +0200

    Updated Schemas

--- hyperledger/part2/part2_chaincode.go
+++ hyperledger/part2/part2_chaincode.go
@@ -183,11 +183,13 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 func (t *SimpleChaincode) readAssetSchemas(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var schemas = `
 	{
-		"API": {
-			"set_mileage": {
-				"description": "Set the current mileage of a marble.",
-				"properties": {
-					"args": {
+	"API": {
+		"set_mileage": {
+			"description": "Set the current mileage of a marble.",
+			"properties": {
+				"args": {
+					"description": "arguments",
+					"items": {
 						"name": {
 							"description": "The name of the marble.",
 							"type": "string"
@@ -196,11 +198,26 @@ func (t *SimpleChaincode) readAssetSchemas(stub *shim.ChaincodeStub, args []stri
 							"description": "The current mileage in km.",
 							"type": "number"
 						}
-					}
-				}
-			}
+					},
+					"maxItems": 2,
+					"minItems": 2,
+					"type": "array"
+				},
+				"function": {
+					"description": "set_mileage function",
+					"enum": [
+						"set_mileage"
+					],
+					"type": "string"
+				},
+				"required": [
+					"name", "mileage"
+				]
+			},
+			"type": "object"
 		}
-	}`
+	}
+}`
 
 	return []byte(schemas), nil
 }
